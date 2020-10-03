e362edccb0ee649245199557947c2952019325f5
Joshua Watson <josh.m.watson@gmail.com>
2020-10-03 08:18:49 -0500

    Updated Sounds and AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public Sound[] Sounds;
+
+        private void Awake()
+        {
+            foreach (Sound s in Sounds)
+            {
+                s.Source = gameObject.AddComponent<AudioSource>();
+                s.Source.clip = s.Clip;
+                s.Source.volume = s.Volume;
+            }
+        }
+
+        private void Start()
+        {
+            Play("FullBand");
+        }
+
+        public void Play(string soundName)
+        {
+            Sound s = Array.Find(Sounds, sound => sound.Name == soundName);
+            s.Source.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [System.Serializable]
+    public class Sound
+    {
+        public string Name;
+
+        public AudioClip Clip;
+
+        [Range(0f,1f)]
+        public float Volume;
+        public float Pitch;
+
+        [HideInInspector]
+        public AudioSource Source;
+    }
+}
