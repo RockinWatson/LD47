6248db9c46e24d0f3c0af3e305057bcee4fc8d09
Joshua Watson <josh.m.watson@gmail.com>
2020-10-02 22:02:51 -0500

    basic sound manager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public static class SoundManager
+    {
+        public enum Sound
+        { 
+            Drums,
+            KeysOne,
+            KeysTwo,
+            Bass
+        }
+
+        public static void PlaySound(Sound sound) {
+            GameObject soundGameObject = new GameObject("Sound");
+            AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+            audioSource.clip = GetAudioClip(sound);
+            audioSource.Play();
+        }
+
+        private static AudioClip GetAudioClip(Sound sound)
+        {
+            foreach (var soundClip in GameAssets.i.SoundClip)
+            {
+                if (soundClip.Sound == sound)
+                {
+                    return soundClip.AudioClip;
+                }
+            }
+            Debug.LogError("Sound " + sound + " not found!");
+            return null;
+        }
+    }
+}
