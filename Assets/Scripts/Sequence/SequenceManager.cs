e488ea8b50130f251d1dda46483e4bc8bce003c7
Greg Raab <greg@recroom.com>
2020-10-05 07:11:54 -0700

    Dumping latest

---
+++ Assets/Scripts/Sequence/SequenceManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceManager : MonoBehaviour
+{
+    [SerializeField] private List<Sequence> _allSequences;
+
+    [SerializeField] private GameObject[] ARROWS_PREFABS = new GameObject[4];
+    [SerializeField] private Vector3[] ARROWS_POS = new Vector3[4];
+
+    [SerializeField] private GameObject TARGET = null;
+    public Vector3 GetTargetPos() { return TARGET.transform.position; }
+    [SerializeField] private float ACTIVE_TARGET_RANGE = 1f;
+    public float GetActiveTargetRange() { return ACTIVE_TARGET_RANGE; }
+
+    [SerializeField] private float ARROW_SPEED = 3f;
+    public float GetArrowSpeed() { return ARROW_SPEED; }
+
+    private float _sequenceTimer = 0f;
+
+    public enum TargetScore
+    {
+        EXCELLENT = 0,
+        GOOD = 1,
+        OKAY = 2,
+        BAD = 3,
+    }
+    [SerializeField] private float[] TARGET_RANGES = new float[4];
+    public float[] GetTargetRanges() { return TARGET_RANGES; }
+
+    static private SequenceManager _instance = null;
+    static public SequenceManager Get() { return _instance; }
+
+    private void Awake()
+    {
+        if(_instance != null)
+        {
+            Debug.LogError("Something fucky happened - should only be 1 SequenceManager");
+            return;
+        }
+
+        _instance = this;
+    }
+
+    private void Update()
+    {
+        //@TODO: Figure out how / when to play a new sequence
+        if(_sequenceTimer <= 0f)
+        {
+            PlayRandomSequence();
+        }
+        else
+        {
+            _sequenceTimer -= Time.deltaTime;
+        }
+
+        //@TEMP: For now, just do it based on key stroke
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            PlayRandomSequence();
+        }
+
+        //@TODO: Maybe also track input for arrows etc and accuracy?
+    }
+
+    private void PlayRandomSequence()
+    {
+        int i = Random.Range(0, _allSequences.Count);
+        var sequence = _allSequences[i];
+
+        PlaySequence(sequence);
+    }
+
+    private void PlaySequence(Sequence sequence)
+    {
+        _sequenceTimer = 0f;
+        float xPos = 0f;
+        foreach(var note in sequence.Arrows)
+        {
+            int index = (int)note.Arrow;
+            GameObject arrow = Instantiate(ARROWS_PREFABS[index], ARROWS_POS[index] + Vector3.right * xPos, Quaternion.identity, this.transform);
+
+            xPos += note.Time;
+            _sequenceTimer += note.Time;
+        }
+        _sequenceTimer *= .75f;
+    }
+}
---
+++ Assets/Scripts/Sequence/SequenceNote.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceNote : MonoBehaviour
+{
+    [SerializeField] private Sequence.Arrow _arrow;
+    [SerializeField] private SpriteRenderer _sprite = null;
+
+    private void Update()
+    {
+        //@TODO: Check input to see if key is hit
+        if(IsInActiveRange())
+        {
+            CheckInput();
+        }
+
+        //@TODO: Move the note along...
+        MoveUpdate();
+    }
+
+    private void CheckInput()
+    {
+        switch(_arrow)
+        {
+            case Sequence.Arrow.UP:
+                if(Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    ProcessHit();
+                }
+                break;
+            case Sequence.Arrow.LEFT:
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    ProcessHit();
+                }
+                break;
+            case Sequence.Arrow.RIGHT:
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    ProcessHit();
+                }
+                break;
+            case Sequence.Arrow.DOWN:
+                if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    ProcessHit();
+                }
+                break;
+            default:
+                Debug.LogError("WTF?");
+                break;
+        }
+    }
+
+    private float GetDeltaXFromTarget()
+    {
+        Vector3 targetPos = SequenceManager.Get().GetTargetPos();
+        Vector3 ourPos = this.transform.position;
+        return Mathf.Abs(targetPos.x - ourPos.x);
+    }
+
+    private bool IsInActiveRange()
+    {
+        return (GetDeltaXFromTarget() <= SequenceManager.Get().GetActiveTargetRange());
+    }
+
+    private void ProcessHit()
+    {
+        //@TODO: Check to see where in the range we are exactly and score the hit
+        var targetRanges = SequenceManager.Get().GetTargetRanges();
+
+        float deltaX = GetDeltaXFromTarget();
+        if(deltaX <= targetRanges[(int)SequenceManager.TargetScore.EXCELLENT])
+        {
+            Debug.Log("EXCELLENT!");
+            _sprite.color = Color.blue;
+        }
+        else if (deltaX <= targetRanges[(int)SequenceManager.TargetScore.GOOD])
+        {
+            Debug.Log("GOOD!");
+            _sprite.color = Color.green;
+        }
+        else if (deltaX <= targetRanges[(int)SequenceManager.TargetScore.OKAY])
+        {
+            Debug.Log("OKAY!");
+            _sprite.color = Color.yellow;
+        }
+        else if (deltaX <= targetRanges[(int)SequenceManager.TargetScore.BAD])
+        {
+            Debug.Log("BAD!");
+            _sprite.color = Color.red;
+        }
+    }
+
+    private void MoveUpdate()
+    {
+        this.transform.Translate(-SequenceManager.Get().GetArrowSpeed() * Time.deltaTime, 0f, 0f);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
